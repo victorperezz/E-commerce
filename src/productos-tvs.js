499f23a3239ae4eb6c684ace412a6e02e13a7de5
victorperezz <vp180402@gmail.com>
2024-07-08 15:26:45 -0600

    Add files via upload

---
+++ src/productos-tvs.js
@@ -0,0 +1,39 @@
+export const productosTvs = [
+    {
+        id: 5,
+        img: "",
+        titulo: "Smart Led Tv Philips 32''",
+        descripcion: "Smart Led Android Tv Philips 32 Pulgadas HD",
+        caracteristicas: "Televisor Smart / Android. Pantalla: TV LED HD. Resolucion de panel: 1366 x 768 p. Relacion de aspecto: 16:9. Optimizacion de la imagen: HDR10, HLG (Hybrid Log Gamma), Pixel Plus alta definicion. Tamano diagonal de pantalla: 32 pulgadas / 80 cm. Cantidad de conexiones HDMI: 3. Cantidad de puertos USB: 2. WIFI. Bluetooth.",
+        precio: 64.499,
+        boton: "Comprar"
+      },
+      {
+        id: 6,
+        img: "",
+        titulo: "Smart Led Tv Quint 42''",
+        descripcion: "Smart Led Tv Quint 42 Pulgadas FULL HD",
+        caracteristicas: "Televisor Smart de 42 FHD (1920x1080), pantalla plana. Tiempo de respuesta 8ms. Frecuencia de barrido 60Hz. Brillo 240cd/m2 Radio de contraste 3000:1. Parlantes integrados. Potencia RMS 2x8W. Dolby MS12. Salidas de audio. Wi Fi. Entrada video componente. 2 Entradas USB. 3 Entradas HDMI.",
+        precio: 59.999,
+        boton: "Comprar"
+      },
+      {
+        id: 7,
+        img: "",
+        titulo: "Smart Led Tv Samsung 42''",
+        descripcion: "Smart Led Tv Samsung 42 Pulgadas FULL HD",
+        caracteristicas: "Smart TV LED 43 FHD 1920 x 1080. Clean View. Wide Color Enhancer. Wi-Fi integ. SO tizen. Dolby Digital Plus. Sonido 20W RMS. Samsung Cloud. Smart View/Hub. HDMI x 2. USB x 1. HDR. Control remoto. Manual.",
+        precio: 83.999,
+        boton: "Comprar"
+      },
+      {
+        id: 8,
+        img: "",
+        titulo: "Smart Neo TV Samsung 42''",
+        descripcion: "Smart Neo QLED TV Samsung 43 Pulgadas 4K",
+        caracteristicas: "Televisor gaming Smart, Neo QLED serie 9 con pantalla de 43, UHD 4K (3840x2160) Formato de pantalla wide. Tasa de refresco 144 Hz. Procesador Neo Quantum 4K. Mil millones de colores. PQI (indice de calidad de la imagen) 4500. Quantum HDR 24x. HLG. Contraste Quantum MatrixTecnhology, Volumen de color al 100 % con Quantum Dot. Deteccion de brillo y color. Atenuacion UHD definitiva. Motion Xcelerator Turbo Pro. LED Clear Motion. Reduccion de ruido. Calibracion experta. Modo cineasta (FMM). Audio Dolby Digital Plus MS12 2ch. Potencia 20W (RMS). Amplificador de voz activo.",
+        precio: 251.999,
+        boton: "Comprar"
+      }
+  ];
+  
